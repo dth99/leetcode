dd061adc975de979e94b3284680f72db55314f55
Deepak kumar <59889069+dth99@users.noreply.github.com>
2022-01-11 12:29:07 +0530

    Time: 54 ms (12.48%), Space: 25.7 MB (10.75%) - LeetHub

--- 189-rotate-array/189-rotate-array.cpp
+++ 189-rotate-array/189-rotate-array.cpp
@@ -4,7 +4,7 @@ public:
         int n=nums.size();
         vector<int> ans(n);
         for(int i=0;i<n;i++){
-            ans[(i+k)%n] = nums[i];
+            ans[(i+k)%n]=nums[i];
         }
         nums=ans;
     }
