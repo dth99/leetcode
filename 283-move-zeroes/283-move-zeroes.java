93e3709abed1f1363e53b84b01cd8f6df248b72e
Deepak kumar <59889069+dth99@users.noreply.github.com>
2022-01-18 15:45:44 +0530

    Time: 3 ms (32.36%), Space: 52.5 MB (23.83%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+    int count = 0;
+    for (int i = 0; i < nums.length; i++) {
+        if (nums[i] == 0) {
+            count++;
+        } else {
+            nums[i - count] = nums[i];
+            if (count != 0) {nums[i] = 0;}
+        }
+    }
+    return;
+    }
+}
