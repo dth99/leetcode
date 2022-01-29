ffd9a13700cc25a3b51b80858b7598ca03203452
Deepak kumar <59889069+dth99@users.noreply.github.com>
2022-01-29 22:01:37 +0530

    Time: 16 ms (69.74%), Space: 11.4 MB (10.34%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
@@ -3,6 +3,7 @@ public:
     int diagonalSum(vector<vector<int>>& mat) {
         int n=mat.size();
         int sum=0;
+        /* brute force (not bad one)
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
                 if(i==j){
@@ -15,5 +16,14 @@ public:
         }
         if(n&1) return sum-mat[n/2][n/2];
         else    return sum;
+        
+        */
+        for(int i=0;i<n;i++){
+            sum+=mat[i][i];
+            if(i!=n-i-1){
+                sum+=mat[i][n-i-1];
+            }
+        }
+        return sum;
     }
 };
