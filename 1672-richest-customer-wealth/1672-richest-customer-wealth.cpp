8286acec3073280db33c5d23c07fe49ec534e7bc
Deepak kumar <59889069+dth99@users.noreply.github.com>
2022-01-31 08:36:35 +0530

    Time: 3 ms (95.59%), Space: 7.7 MB (99.29%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
@@ -2,7 +2,8 @@ class Solution {
 public:
     int maximumWealth(vector<vector<int>>& accounts) {
         int maxWealth=0;
-        for(int i=0;i<accounts.size();i++){
+        int r=accounts.size();
+        for(int i=0;i<r;i++){
             int wealth=0;
             for(int j=0;j<accounts[i].size();j++){
                 wealth+=accounts[i][j];
