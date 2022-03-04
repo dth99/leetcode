e323f58b7209b5ed53b5a9dff20d7190561e19ce
Deepak kumar <59889069+dth99@users.noreply.github.com>
2022-03-04 08:44:51 +0530

    Time: 15 ms (63.73%), Space: 7.6 MB (39.91%) - LeetHub

---
+++ 237-delete-node-in-a-linked-list/237-delete-node-in-a-linked-list.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        // if node is last node of ll
+        if(node->next==NULL) delete node;
+        
+        // swapping current node (node want to remove) with next node
+        swap(node->val,node->next->val);
+        ListNode* temp=node->next; // storing the node we want to delete
+        
+        node->next=node->next->next;
+        delete temp;
+    }
+};
